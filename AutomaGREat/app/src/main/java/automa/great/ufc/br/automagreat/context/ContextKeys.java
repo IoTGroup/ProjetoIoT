ea17d25d170da1a40e43682dcc3005665761a74c
lanabia <lanabeatriz.mesquita@gmail.com>
2015-10-21 16:07:16 -0300

    Adição de arquivos do Loccam, ainda não funcionam

---
+++ AutomaGREat/app/src/main/java/automa/great/ufc/br/automagreat/context/ContextKeys.java
@@ -0,0 +1,10 @@
+package automa.great.ufc.br.automagreat.context;
+
+/**
+ * Created by great on 03/07/2015.
+ */
+public class ContextKeys {
+
+    public static final String HUE_LIGHT = "control.ambient.light.hue";
+    
+}
--- AutomaGREat/app/src/main/java/automa/great/ufc/br/automagreat/view/activity/TabsActivity.java
+++ AutomaGREat/app/src/main/java/automa/great/ufc/br/automagreat/view/activity/TabsActivity.java
@@ -12,16 +12,23 @@ import android.view.View;
 import android.widget.Toast;
 
 import automa.great.ufc.br.automagreat.R;
+import automa.great.ufc.br.automagreat.context.ContextKeys;
 import automa.great.ufc.br.automagreat.view.layout.SlidingTabLayout;
 import automa.great.ufc.br.automagreat.view.adapter.ViewPagerAdapter;
+import br.ufc.great.loccamlib.LoccamListener;
+import br.ufc.great.loccamlib.LoccamManager;
+import br.ufc.great.syssu.base.Tuple;
+import br.ufc.great.syssu.base.interfaces.ISysSUService;
 
-public class TabsActivity extends ActionBarActivity {
+public class TabsActivity extends ActionBarActivity implements LoccamListener{
     private Toolbar toolbar;
     private ViewPager pager;
     private ViewPagerAdapter adapter;
     private SlidingTabLayout tabs;
     private CharSequence Titles[]={"Lamps","Air Conditioning"};
     private int Numboftabs =2;
+
+    private LoccamManager loccam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,12 @@ public class TabsActivity extends ActionBarActivity {
 
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
+
+        loccam = new LoccamManager(this, "automagreat");
+        loccam.connect(this);
+
+
+
     }
 
     @Override
@@ -76,11 +89,64 @@ public class TabsActivity extends ActionBarActivity {
         }
     }
 
-    /*protected void onListItemClick(ListView l, View v, int position, long id) {
-        // TODO Auto-generated method stub
-        super.onListItemClick(l, v, position, id);
+    @Override
+    public void onServiceConnected(ISysSUService iSysSUService) {
+
+        loccam.init(ContextKeys.HUE_LIGHT);
+        Log.d("Automa","onServiceConnected aeeee");
+
+    }
+
+    @Override
+    public void onServiceDisconnected() {
+
+        loccam.finishAll();
+        Log.d("Automa","onServiceDisconnected");
+
+    }
+
+    @Override
+    public void onLoccamException(Exception e) {
+        Log.d("Automa","Erro: "+e.toString());
+    }
 
 
-    }*/
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        loccam.disconnect();
+
+    }
+
+    public void turnOn(View view) {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("State", "on");
+        loccam.setASync(tuple);
+        Log.d("Automa","Ligando");
+    }
+
+    public void turnOn() {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("State", "on");
+        loccam.setASync(tuple);
+    }
+
+    public void turnOff() {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("State", "off");
+        loccam.setASync(tuple);
+    }
+
+    public void setColor(String color) {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("Color", color);
+        loccam.setASync(tuple);
+    }
+
+    public void setBright(String bright) {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("Brightness", bright);
+        loccam.setASync(tuple);
+    }
+
+    public void connect(String lampId) {
+        Tuple tuple = (Tuple) new Tuple().addField("ControlKey", ContextKeys.HUE_LIGHT).addField("Light", lampId);
+        loccam.setASync(tuple);
+    }
 
 }
